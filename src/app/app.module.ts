98af245c6ab3f0f0587bda85ffb0c78a1c7dbfed
karim bouzid <kaarimbouzid@gmail.com>
2021-01-09 22:37:10 +0100

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,26 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { SearchTrainingComponent } from './search-training/search-training.component';
-import { LoginModule } from './modules/login/login.module';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './modules/authentification/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    SearchTrainingComponent
+    SearchTrainingComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    LoginModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/authentification/authentification.module.ts
+++ src/app/modules/authentification/authentification.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MaterialModule } from 'src/app/material/material.module';
 
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [LoginComponent]
 })
 export class AuthentificationModule { }
